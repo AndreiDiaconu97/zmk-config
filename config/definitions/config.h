22395f228c240c9c1f6f20e2d03cefa77e485d44
AndreiDiaconu97 <themiticadi@gmail.com>
2023-03-24 23:54:16 +0100

    refactoring fixes

--- config/definitions/config.h
+++ config/definitions/config.h
@@ -32,8 +32,8 @@
 // };
 
 &caps_word {
-    /delete-property/ ignore-modifiers;
-    continue-list = <UNDERSCORE MINUS BSPC DEL N1 N2 N3 N4 N5 N6 N7 N8 N9 N0>;
+  /delete-property/ ignore-modifiers;
+  continue-list = <UNDERSCORE MINUS BSPC DEL N1 N2 N3 N4 N5 N6 N7 N8 N9 N0>;
 };
 
 /*                KEY POSITIONS
