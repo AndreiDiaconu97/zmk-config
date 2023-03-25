c97ce949210ddfbde86e902cc2989ce9c60b047b
AndreiDiaconu97 <themiticadi@gmail.com>
2023-03-25 23:46:51 +0100

    try mouse turby click tap dance

--- config/definitions/config.h
+++ config/definitions/config.h
@@ -32,7 +32,7 @@
 // };
 
 &caps_word {
-  // /delete-property/ ignore-modifiers;
+  /delete-property/ ignore-modifiers;
   continue-list = <UNDERSCORE MINUS BSPC DEL N1 N2 N3 N4 N5 N6 N7 N8 N9 N0>;
 };
 
